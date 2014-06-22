a3fd30c5e874392e4a1de4b80e6b4c517ac8dbef
Zack Fisch <zack@zfisch.com>
2014-06-22 11:59:32 -0400

    Fixing merge

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,8 @@ class HomeController < ApplicationController
 
   def intro
   	@user = User.new
+    invoice = $client.post 'invoice', {:price => 10.00, :currency => 'USD'}
+    @url = invoice["url"].to_s + '&view=iframe'
   end
 
-  def bitcoin_donation
-  	@user = User.new
-  	invoice = $client.post 'invoice', {:price => 10.00, :currency => 'USD'}
-  	binding.pry
-  	@url = invoice["url"].to_s + '&view=iframe'
-  	render :intro
-  end
 end
