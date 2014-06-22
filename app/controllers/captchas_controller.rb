2c312a3ee84ff154b955475dfcf1b362ff53d8ce
Josh Cornelius <joshcornelius9@gmail.com>
2014-06-21 23:43:37 -0400

    some stuff

--- app/controllers/captchas_controller.rb
+++ app/controllers/captchas_controller.rb
@@ -1,54 +1,43 @@
 
 
 class CaptchasController < ApplicationController
-	before_filter :authenticate
+	# before_filter :authenticate, except: [:status]
 
 	def js
-		begin
-		  Captcha.authenticate_public(params[:k])
-		  send_file 'public/.....', type: 'text/javascript'
-		rescue 
-			render nothing: true, status: 420
-		end
+		 send_file 'public/.....', type: 'text/javascript'
 	end
 
 	def get
-		begin
-		  Captcha.authenticate_public(params[:k])
-		  @captcha = Captcha.gif()
-			User.find_by(pub_key: params[:k]).captcha.new(sequence: @captcha[:sequence], image: @captcha[:gif], read: false)
-			render :json => { id: @captcha.id, image: @captcha.image }, status: 200
-		rescue
-			render nothing: true, status: 420
-		end
+		@captcha = Captcha.gif()
+		User.find_by(pub_key: params[:k]).captcha.new(sequence: @captcha[:sequence], image: @captcha[:gif], read: false)
+		render :json => { id: @captcha.id, image: @captcha.image }, status: 200
 	end
 
 	def check
-		begin 
-			Captcha.authenticate_public(params[:k])
-			requested_captcha = Captcha.find(params[:id])
-			if params[:s] == requested_captcha.sequence
-				render :json => { success: true }, status: 200
-			else 
-				render :json => { success: false }, status: 200
-			end
-		rescue
-			render nothing: true, status: 420
+		requested_captcha = Captcha.find(params[:id])
+		if Captcha.authenticate_public(params[:id]) && params[:s] == requested_captcha.sequence
+			render json: { success: true }, status: 200
+		else
+			render json: { success: false }, status: 200
 		end
 	end
 
 	def status
-		begin
-			Captcha.authenticate_private(params[:k])
-			if Captcha.find(params[:id]).read?
-				render nothing: true, status: 420
-			else
-				render :json => { success: Captcha.find(params[:id]).success }, status: 200
-			end
-		rescue
+		requested_captcha = Captcha.find(params[:id])
+		if Captcha.authenticate_private(params[:k]) && requested_captcha.read?
+			render :json => { success: Captcha.find(params[:id]).success }, status: 200
+		else
 			render nothing: true, status: 420
 		end
 		requested_captcha.status = 'read'
+	end
+
+private
+
+	def authenticate
+		unless Captcha.authenticate_public(params[:k])
+			render nothing: true, status: 420
+		end
 	end
 
 end
